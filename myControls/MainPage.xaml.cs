04b4078394fbd356f7378418bb299e416b7d6174
marti <marti@CASEOFF>
2025-03-12 10:54:28 +0100

    Projektdateien hinzufügen.

---
+++ myControls/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace myControls
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+            birthdatePicker.Date = DateTime.Now;
+        }
+
+
+        private void OnSendButtonClicked(object sender, EventArgs e)
+        {
+            // Einfaches Beispiel: Werte aus Entry und Editor auslesen
+            string username = usernameEntry.Text;
+            string comment = commentEditor.Text;
+
+            // Wert aus Picker
+            string selectedCountry = countryPicker.SelectedItem?.ToString() ?? "Kein Land ausgewählt";
+
+            // Wert aus DatePicker
+            DateTime selectedDate = birthdatePicker.Date;
+
+            // Wert aus TimePicker
+            TimeSpan selectedTime = reminderTimePicker.Time;
+
+            // Hier könntest du z.B. eine einfache Ausgabe per DisplayAlert machen
+            DisplayAlert(
+                "Eingaben",
+                $"Name: {username}\nKommentar: {comment}\nLand: {selectedCountry}\n" +
+                $"Datum: {selectedDate.ToShortDateString()}\n" +
+                $"Uhrzeit: {selectedTime}",
+                "OK");
+
+
+        }
+
+
+
+    }
+
+}
---
+++ myControls/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace myControls;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
